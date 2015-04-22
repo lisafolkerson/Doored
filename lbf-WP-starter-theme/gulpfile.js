2d129de16a3a1df8947d9eb4a809dbc37ad8173d
Lisa Folkerson <lisafolkerson@gmail.com>
2015-04-22 09:59:29 -0400

    gulp and sass

--- lbf-WP-starter-theme/gulpfile.js
+++ lbf-WP-starter-theme/gulpfile.js
@@ -5,12 +5,12 @@ var notify = require ( 'gulp-notify' );
 
 // Here we define the list of things to happen when we run gulp styles
 gulp.task( 'styles', function() {
-	gulp.src( 'wp-content/themes/LBF-Doored-Theme/' )
+	gulp.src( 'wp-content/themes/LBF-Doored-Theme/styles/styles.scss' )
 	.pipe( sass({errLogToConsole : true}))
-	.pipe( gulp.dest('wp-content/themes/contemporary_art_gallery/' ))
+	.pipe( gulp.dest('wp-content/themes/LBF-Doored-Theme/' ))
 	.pipe( notify ( 'CSS Compiled' ));
 	});
 
 gulp.task( 'watch', function() {
-	gulp.watch('wp-content/themes/contemporary_art_gallery/*.scss',['styles'])
+	gulp.watch('wp-content/themes/LBF-Doored-Theme/**/*.scss',['styles'])
 	});
