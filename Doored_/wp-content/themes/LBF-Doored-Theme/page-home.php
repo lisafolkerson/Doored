e9b4f330d34d8c36f4563edd83c059360d62806c
Lisa Folkerson <lisafolkerson@gmail.com>
2015-05-15 16:31:15 -0400

    beauty adding to front page

--- Doored_/wp-content/themes/LBF-Doored-Theme/page-home.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/page-home.php
@@ -13,14 +13,16 @@ get_header();  ?>
 		<div class="mainLeft">
 	
 			<!-- THE IMAGE FOR LIVE STREAM -->
-		  <?php $image = get_field('livestream');
-		  if( !empty($image) ): ?>
-		  	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-		  <?php endif; ?>
+		  <div class="livestream">	
+			  <?php $image = get_field('livestream');
+			  if( !empty($image) ): ?>
+			  	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+			  <?php endif; ?>
+		  </div><!--end .livestream-->
 
 		  <h2 class="pageTitle"><?php the_title(); ?></h2>
 
-		  <p><?php the_field('subtitle') ?></p>
+		  <p class="subtitle"><?php the_field('subtitle') ?></p>
 
 		   <!-- THE IMAGE TO REPRESENT DOORED -->
 		  <?php $dooredImg = get_field('main_image');
