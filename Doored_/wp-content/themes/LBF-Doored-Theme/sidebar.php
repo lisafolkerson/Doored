69716cc432e710ba2225e190afed9f936ee24189
Lisa Folkerson <lisafolkerson@gmail.com>
2015-06-03 00:08:59 -0400

    if statement sidebargit add -A :/git add -A :/

--- Doored_/wp-content/themes/LBF-Doored-Theme/sidebar.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/sidebar.php
@@ -2,6 +2,17 @@
 	<ul class="xoxo">
 		<h2 class="sidebarTitle">Artists</h2>
 		<?php  dynamic_sidebar( 'primary-widget-area' ); ?>
+
+		<?php 
+
+		if ( is_single() || is_page(shows) ) : // artist page?>
+		           <h1 class="entry-title" itemprop="name"><?php the_title(); ?> or Shows</h1>
+		       <?php else : ?>     
+		           <h2 class="entry-title" itemprop="name">
+		           	or artists
+		               <a itemprop="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		           </h2>
+		       <?php endif; ?>
 	</ul>
 </div>
 	
