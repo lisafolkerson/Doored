3009efb3644d0bad61e341538eaacb81fbfae1f6
Lisa Folkerson <lisafolkerson@gmail.com>
2015-06-06 01:15:10 -0400

    more and more

--- Doored_/wp-content/themes/LBF-Doored-Theme/sidebar.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/sidebar.php
@@ -1,8 +1,8 @@
 <div class="sidebar">
 	<ul class="xoxo">
+			<h3 class="sidebarTitle" itemprop="name"><?php the_title(); ?></h3>
 		<?php 
 		if ( is_page( 'shows' ) || is_single( 'show' ) ) : ?>
-			<h3 class="sidebarTitle" itemprop="name"><?php the_title(); ?></h3>
 			<?php $latestPosts = new WP_Query(array(
 			    'post_type' => 'show', //only want show posts
 			)) ?>			
@@ -10,13 +10,16 @@
 			  <div class="single-entry entry-show">
 			    <?php $showImg = get_field('show_image'); ?>
 			    <img src="<?php echo $showImg['sizes']['square']; ?>" alt="<?php echo $showImg['alt']; ?>" class="showImage">
-			    <h3 class="entry-title"><?php the_title(); ?></h3>
+			    <p class="entry-title"><?php the_title(); ?></p>
 			  </div><!--end .single-entry-->		    
 			  <?php endwhile //end custom loop ?>
 		<?php endif; ?>	    
+
+ <?php wp_reset_postdata(); //return env back to regular functionality?>
+
+
 		<?php 
-		if ( is_page( 'artists' ) || is_single( 'artist' ) ) : ?>  
-		    <h3 class="sidebarTitle" itemprop="name"><?php the_title(); ?></h3>         
+		if ( is_page( 'artists' ) || is_single( 'artist' ) ) : ?>        
 	      <?php $latestPosts = new WP_Query(array(
 	          'post_type' => 'artist', //only want artist posts
 	      )) ?>
@@ -26,11 +29,12 @@
         <div class="single-entry entry-artist">
         	<?php $artistImg = get_field('artist_image'); ?>
         	<img src="<?php echo $artistImg['sizes']['square']; ?>" alt="<?php echo $artistImg['alt']; ?>" class="artistImage">
-          <h3 class="entry-title"><?php the_title(); ?></h3>
+          <p class="entry-title"><?php the_title(); ?></p>
         </div><!--end .single-entry-->
       
 
 		      <?php endwhile //end custom loop ?>
 		<?php endif; ?>
+	 <?php wp_reset_postdata(); //return env back to regular functionality?>
 	</ul>
 </div>
