3aea07364b3472b48d0254c436d2a34fd6428d5e
Lisa Folkerson <lisafolkerson@gmail.com>
2015-06-06 00:34:18 -0400

    oh ya dat sweet sidebar

--- Doored_/wp-content/themes/LBF-Doored-Theme/sidebar.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/sidebar.php
@@ -1,44 +1,36 @@
 <div class="sidebar">
 	<ul class="xoxo">
 		<?php 
-
 		if ( is_page( 'shows' ) ) : ?>
-			<h3 class="entry-title" itemprop="name"><?php the_title(); ?></h3>
+			<h3 class="sidebarTitle" itemprop="name"><?php the_title(); ?></h3>
 			<?php $latestPosts = new WP_Query(array(
 			    'post_type' => 'show', //only want show posts
 			)) ?>			
 				<?php if ($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post(); ?>
-			  <div class="single-entry">
-			    <h3><?php the_title(); ?></h3>
-			    <p class="entry-title"><?php the_field('about_show'); ?></p>
-
+			  <div class="single-entry entry-show">
 			    <?php $showImg = get_field('show_image'); ?>
-			    <img src="<?php echo $showImg['url']; ?>" alt="<?php echo $showImg['alt']; ?>" class="showImage">
-			  </div><!--end .single-entry-->
-			    
+			    <img src="<?php echo $showImg['sizes']['square']; ?>" alt="<?php echo $showImg['alt']; ?>" class="showImage">
+			    <h3 class="entry-title"><?php the_title(); ?></h3>
+			  </div><!--end .single-entry-->		    
 			  <?php endwhile //end custom loop ?>
-			  <?php// wp_reset_postdata(); //return env back to regular functionality?>
-
-		<?php endif; ?>	
-
+		<?php endif; ?>	    
 		<?php 
-
 		if ( is_page( 'artists' ) ) : ?>  
-		    <h3 class="entry-title" itemprop="name"><?php the_title(); ?></h3>         
+		    <h3 class="sidebarTitle" itemprop="name"><?php the_title(); ?></h3>         
+	      <?php $latestPosts = new WP_Query(array(
+	          'post_type' => 'artist', //only want artist posts
+	      )) ?>
 
-		    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-		      <?php $latestPosts = new WP_Query(array(
-		          'post_type' => 'artist', //only want artist posts
-		      )) ?>
+		    <?php if ($latestPosts->have_posts()) while($latestPosts->have_posts()) : $latestPosts->the_post(); ?>
 		    
-		          <div class="single-entry">
-		            <h1 class="entry-title"><?php the_title(); ?></h1>
-		          </div><!--end .single-entry-->
-		        
+        <div class="single-entry entry-artist">
+        	<?php $artistImg = get_field('artist_image'); ?>
+        	<img src="<?php echo $artistImg['sizes']['square']; ?>" alt="<?php echo $artistImg['alt']; ?>" class="artistImage">
+          <h3 class="entry-title"><?php the_title(); ?></h3>
+        </div><!--end .single-entry-->
+      
 
 		      <?php endwhile //end custom loop ?>
-		      <?php //wp_reset_postdata(); //return env back to regular functionality?>       
 		<?php endif; ?>
 	</ul>
 </div>
