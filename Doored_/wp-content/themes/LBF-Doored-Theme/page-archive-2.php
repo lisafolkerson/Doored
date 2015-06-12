02e85f4594dbfda57ba513cf57f2cd1c1e052ac9
Lisa Folkerson <lisafolkerson@gmail.com>
2015-06-12 11:34:43 -0400

    archive loops

--- Doored_/wp-content/themes/LBF-Doored-Theme/page-archive-2.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/page-archive-2.php
@@ -7,25 +7,23 @@
     <h2>ARCHIVE EH</h2>
 
         <!-- bring in all images from uploaded media -->
-<?php
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post();    
+        $query_images_args = array(
+            'post_type' => 'attachment', 'post_mime_type' =>'image', 'post_status' => 'inherit', 'posts_per_page' => -1,
+        );
 
-    $query_images_args = array(
-        'post_type' => 'attachment', 'post_mime_type' =>'image', 'post_status' => 'inherit', 'posts_per_page' => -1,
-    );
-
-    $query_images = new WP_Query( $query_images_args );
-    $images = array();
-    foreach ( $query_images->posts as $image) {
-        $images[]= wp_get_attachment_url( $image->ID );
-    }
-
-    $list = array();
-    foreach ($query_images_args->posts as $post) {
-        $list[] = wp_get_attachment_url($post->ID);
-    }
-    ?>
-    <img src="<?php echo $images; ?>" class="allImages">
-
+        $query_images = new WP_Query( $query_images_args );
+        $images = array();
+        foreach ( $query_images->posts as $image) {
+            $images[]= wp_get_attachment_url( $image->ID );
+        }
+        ?>
+    
+    <?php foreach ($images as $img) { ?>
+      <img src="<?php echo $img; ?>" class="allImages">
+    <?php } ?>
+           
+           <?php endwhile; endif; ?>
     </div><!--/content-->
   </div> <!-- /.container -->
 </div> <!-- /.main -->
