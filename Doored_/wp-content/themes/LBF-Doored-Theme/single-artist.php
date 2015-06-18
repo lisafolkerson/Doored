d1b7f15817ba2c6bbcde4a40e339b26288e3121a
Lisa Folkerson <lisafolkerson@gmail.com>
2015-06-17 23:30:34 -0400

    gallery working

--- Doored_/wp-content/themes/LBF-Doored-Theme/single-artist.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/single-artist.php
@@ -31,7 +31,34 @@
 
       <?php endwhile; // end of the loop. ?>
 
-      <?php the_sub_field('artist_gallery');?>
+
+      <div class="gallery clearfix">
+        <?php
+        // check if the repeater field has rows of data
+        if( have_rows('artist_gallery') ):
+          // loop through the rows of data
+            while ( have_rows('gallery') ) : the_row();
+            $rptrImg = get_field('artist_image'); ?>
+
+            <div class="pinnedItem">
+              <a href="<?php the_sub_field( 'links_to' ); ?>">
+                <img src="<?php echo $rptrImg['url']; ?>" alt="<?php echo $rptrImg['alt']; ?>" class="repeatingArtistImage"/>
+              
+                  <p><?php the_sub_field('artist_text');?></p>
+                </a>
+              </div><!--end .pinnedItem-->
+
+            <?php endwhile;
+
+        else :
+            // no rows found
+        endif;
+        ?>
+        </div><!--end .repeater-->
+
+
+
+      <?php the_field('artist_gallery');?>
 
       <div class="repeater clearfix" id="repeater">
         <?php
