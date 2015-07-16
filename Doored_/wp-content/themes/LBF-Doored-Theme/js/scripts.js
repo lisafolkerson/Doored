ef2e929e23db7734998d908c78d2cbaf06494b67
Lisa Folkerson <lisafolkerson@gmail.com>
2015-07-16 00:23:54 -0400

    jquery for de dropdown

--- Doored_/wp-content/themes/LBF-Doored-Theme/js/scripts.js
+++ Doored_/wp-content/themes/LBF-Doored-Theme/js/scripts.js
@@ -1,6 +1,15 @@
 $(function(){
 	//your jQuery here
 
+	// reveal menus
+	$( '.menu ul li').click(function () {
+		console.log('fine children');
+	    // $header = $(this);
+	    // $content = $header.next();
+	    // $content.slideToggle(500, function () {
+	    // });     
+	});
+
 // M A S O N R Y 
 jQuery(document).ready(function($){
   $(".repeater").masonry({
@@ -8,6 +17,7 @@ jQuery(document).ready(function($){
     gutter: 15
   });
 });
+
 
 
 }); // end  docReady
