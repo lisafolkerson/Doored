193d3e9ebd7af4f9f094d8ffe4e44fb0745f4d75
Lisa Folkerson <lisafolkerson@gmail.com>
2016-01-23 15:12:09 -0500

    404 page styled

--- Doored_/wp-content/themes/LBF-Doored-Theme/404.php
+++ Doored_/wp-content/themes/LBF-Doored-Theme/404.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
 
 <div class="main">
-  <div class="container">
+  <div class="container  error404">
 
-    <h1>Not Found</h1>
-    <p>Apologies, but the page you requested could not be found. Perhaps searching will help.</p>
-    <?php get_search_form(); ?>
-
-    <img src="<?php bloginfo('template_directory'); ?>/assets/images/tester content/doored_website_ghost.svg" alt="A terrifying ghost">
+    <h1 class="error_h1">404 Sorry Not Found</h1>
+	<div class="ghostContainer">
+	    <img src="<?php bloginfo('template_directory'); ?>/assets/images/tester content/doored_website_ghost.svg" alt="A terrifying ghost" class="404-ghost">
+	</div>
   </div> <!-- /.container -->
 </div> <!-- /.main -->
 
