5b6132710b344caf8bfd6fbd4233206ef3400220
Lisa Folkerson <lisafolkerson@gmail.com>
2016-02-01 17:57:20 -0500

    polish and sparkle

--- Doored_/wp-content/themes/LBF-Doored-Theme/js/scripts.js
+++ Doored_/wp-content/themes/LBF-Doored-Theme/js/scripts.js
@@ -76,16 +76,13 @@ $(function(){
   });
 
 
-  // SORT ARTISTS MENU
-
-
-
-var mylist = $('.artistMenu .sub-menu');
-var listitems = mylist.children('li').get();
-listitems.sort(function(a, b) {
-   return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
-})
-$.each(listitems, function(idx, itm) { mylist.append(itm); });
+  // SORT ARTISTS MENU ALPHABETICALLY
+  var mylist = $('.artistMenu .sub-menu');
+  var listitems = mylist.children('li').get();
+  listitems.sort(function(a, b) {
+     return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+  })
+  $.each(listitems, function(idx, itm) { mylist.append(itm); });
 
 
 }); // end  docReady
